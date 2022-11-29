b7f0920bc124a214e0835b0c737f32b7a17556e3
Frenchman92 <frenchman9218@gmail.com>
2022-11-29 22:17:19 +0300

    Start HOG game

---
+++ Assets/Scripts/GameItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+
+namespace HOG
+{
+    public class GameItem : MonoBehaviour
+    {
+        [SerializeField] private float scaleFactor = 1.5f;
+        [SerializeField] private float scaleDuration = 1.5f;
+        private SpriteRenderer _spriteRenderer;
+
+        private void Awake()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        private void OnMouseUpAsButton()
+        {
+            Find();
+        }
+
+        private void Find()
+        {
+            transform.DOScale(transform.localScale * scaleFactor, scaleDuration).OnComplete(
+                () => _spriteRenderer.DOFade(0, scaleDuration).OnComplete(
+                    () => gameObject.SetActive(false) ));
+        }
+        
+    }
+}
