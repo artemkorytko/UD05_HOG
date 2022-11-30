cf06a4c650ec048b44f2c4f2356f59fa12895e07
vovaB <boltochko123@gmail.com>
2022-11-30 21:05:00 +0300

    add: script(gameItem), prefabs(level, gameItem)

---
+++ Assets/Scripts/GameItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace HOG
+{
+    public class GameItem : MonoBehaviour
+    { 
+        [SerializeField] private float _scaleDuration = 0.5f;
+        [SerializeField] private float _scaleFactor = 1.5f;
+        private SpriteRenderer _spriteRenderer;
+        private Sequence mySequence = DOTween.Sequence();//????
+        private void Awake()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        private void OnMouseUpAsButton()
+        {
+            Find();
+        }
+
+        private void Find()
+        {
+            transform.DOScale(transform.localScale * _scaleFactor, _scaleDuration).OnComplete(
+                () => _spriteRenderer.DOFade(0,_scaleDuration).OnComplete(
+                    ()=> gameObject.SetActive(false)));
+        }
+    }
+}
