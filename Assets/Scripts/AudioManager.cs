24e75e7c615066f4a28e7c7085ef57e8f6617e02
Teistail <teistail@gmail.com>
2022-12-07 22:26:42 +0300

    save scenes

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace HOG
+{
+    public class AudioManager : MonoBehaviour
+    {
+        private AudioSource _audioSource;
+
+        private void Awake()
+        {
+            _audioSource = GetComponent<AudioSource>();
+        }
+
+        private void Start()
+        {
+            
+        }
+
+        private void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+                _audioSource.Play();
+                _audioSource.PlayOneShot(clip);
+        }
+    }
+}
